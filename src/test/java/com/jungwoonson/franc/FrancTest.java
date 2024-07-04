ecd940006be8acc309c53d2a495004df8d8a56a9
jungwoonson <jungwoon727@gmail.com>
2024-07-04 14:50:28 +0900

    test: Franc 테스트 추가

---
+++ src/test/java/com/jungwoonson/franc/FrancTest.java
@@ -0,0 +1,21 @@
+package com.jungwoonson.franc;
+
+import org.junit.jupiter.api.Test;
+
+class FrancTest {
+
+    @Test
+    public void testMultiplication() {
+        Franc five = new Franc(5);
+        Franc product = five.times(2);
+        assertEquals(new Franc(10), product);
+        product = five.times(3);
+        assertEquals(new Franc(15), product);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+}
